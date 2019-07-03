1919e3a677cccf0ebc835969c38b648fdbe7c67e
Goktug Erce <goktugercegurel@gmail.com>
2019-07-04 00:48:30 +0200

    Add requirements.txt and use it on setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name='fantasy-premier-league-stats',
     version='0.0.1',
@@ -14,8 +17,13 @@ setuptools.setup(
     url='https://github.com/goktugerce/fantasy-premier-league',
     packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-    ]
+    ],
+    install_requires=requirements
 )
