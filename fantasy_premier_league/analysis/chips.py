ead9feef98caff2b3ad72fa9fcda9d2f214f6e13
Goktug Erce Gurel <goktugercegurel@gmail.com>
2020-08-02 12:45:47 +0300

    Add seasonal chip usage

--- fantasy_premier_league/analysis/chips.py
+++ fantasy_premier_league/analysis/chips.py
@@ -13,3 +13,17 @@ def get_weekly_chip_usage(teams, percentage=True):
         chips_used = helpers.convert_to_percentage(chips_used, total_teams)
 
     return helpers.sort_by_value(chips_used)
+
+
+def get_season_chip_usage(teams, percentage=True):
+    """Get seasonal chip usage for teams"""
+    chips_used = defaultdict(int)
+    for team in teams:
+        for chip in team.chips:
+            chips_used[chip['name']] += 1
+    
+    if percentage:
+        total_teams = len(teams)
+        chips_used = helpers.convert_to_percentage(chips_used, total_teams)
+
+    return helpers.sort_by_value(chips_used)
