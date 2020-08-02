d2433640cdf393c0dc13eb87ef8d52dc1005850e
Goktug Erce Gurel <goktugercegurel@gmail.com>
2020-08-02 12:37:09 +0300

    Fix player metadata API change

--- fantasy_premier_league/helpers/player.py
+++ fantasy_premier_league/helpers/player.py
@@ -4,10 +4,10 @@ from .models import Player
 
 def download_players(game):
     """Download latest player information."""
-    response = utilities.get_request(utilities.get_url('PLAYERS_METADATA_URL', game))
+    response = utilities.get_request(utilities.get_url('BOOTSTRAP_STATIC_URL', game))
     players = []
 
-    for player in response:
+    for player in response['elements']:
         players.append(Player.Player(player, game=game))
     players.sort(key=lambda p: p.ownership, reverse=True)
     return players
